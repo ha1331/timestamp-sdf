89458be16fc466e4b7cfb3e0b6cb5f7033276f60
Henrik Andersson <henrik.j.andersson@gmail.com>
2017-05-01 10:07:24 +0300

    sdfgdfgsdfg

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express   = require('express');
 const app       = express();
 const moment    = require("moment");
 
+const port = 3000;
+
 app.get('/:param', function (req, res) {
   res.send(req.param);
 });
@@ -9,6 +11,6 @@ app.get('/', function (req, res) {
   res.send("asdafs");
 });
 
-app.listen(80, function () {
-  console.log("Example app listening on port '80'");
+app.listen(port, function () {
+  console.log(`Example app listening on port '${port}'`);
 });
